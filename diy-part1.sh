8ce54dce5ca933df1ed99ded58dc631c11b1786a
lihonfu <50908574+lihonfu@users.noreply.github.com>
2026-09-09 20:42:17 +0800

    Fix: Remove duplicate helloworld feed before adding new one

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,7 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+# Add a feed source (删除重复的 helloworld，然后添加新的)
+sed -i '/helloworld/d' feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
